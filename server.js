590ba264f3a83db01824fd7c1e251084fd0d43b0
m-wie-mischa <mischa.google@gmx.de>
2025-04-01 13:41:15 +0200

    Did many things

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 const port = 3000
+const allowedOrigins = ['http://ghsPoker.github.io/']
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        credentials: true, // If using cookies or authentication
+        })
+    );
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/log-in/', (req, res) => {
+    //compare credentials to MongoDB
+    res.send('MongoDB connection Port')
+})
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
