1570d7ec159c76f6b1d6dd89c2a2ea7306f56654
m-wie-mischa <mischa.google@gmx.de>
2025-04-02 10:51:36 +0200

    Changed import order to look pretty :3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 const {MongoClient} = require('mongodb')
+require('dotenv').config();
 
 const app = express()
 const port = process.env.PORT || 8080;
