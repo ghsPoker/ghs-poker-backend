26f1988f18973461ff5e76e4eba8450080e799a9
m-wie-mischa <mischa.google@gmx.de>
2025-04-06 14:22:45 +0200

    Sign up route works

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const {MongoClient} = require('mongodb')
+const bcrypt = require('bcryptjs');
+const { hash } = require('bcryptjs');
 let db;
 require('dotenv').config();
 
@@ -40,15 +42,30 @@ app.post('/user/sign-up/', (req, res) => {
     console.log('POST request on path "/user/sign-up"');
     //some mongodb stuff
     const receivedData = req.body
-    if (!receivedData.username || !receivedData.password) res.status(400).json({"message": "Missing username or password", "data": receivedData})
-    
-    console.log("before async");
+    if (!receivedData.username || !receivedData.password || receivedData.username.length < 3 || receivedData.password.length < 8) {
+        res.status(400).json({"message": "Invalid username or password", "data": receivedData});
+        console.log("Invalid username or password")
+        return;
+    }
+
+    let user;
     (async () => {
-        console.log("async");
-        const user = await db.findOne({"username": receivedData.username});
+        user = await db.findOne({"username": receivedData.username})
+        if (user) {
+            console.log(user)
+            console.log(receivedData.username)
+            res.status(400).json({"message": "Username already taken", "data": receivedData});
+            console.log("Username already taken")
+            return;
+        }
     })();
 
-    console.log(user);
+    
+    (async () => {
+        await db.insertOne({"username": receivedData.username, "password": bcrypt.hashSync(receivedData.password)})
+        res.status(201).json({"message": "Successfully created account", "data": receivedData})
+        console.log("Successfully created account")
+    })();
 })
 
 app.listen(port, async () => {
@@ -73,3 +90,12 @@ process.on('SIGTERM', async () => {
     await client.close();
     process.exit(0);
 });
+
+
+async function awaitFindOne(query) {
+    return await db.findOne(query)
+}
+
+async function awaitInsertOne(document) {
+    return await db.insertOne(document)
+}
